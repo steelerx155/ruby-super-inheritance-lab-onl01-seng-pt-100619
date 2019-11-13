42cc21b1003a16e0b3d4f7d00669da35d853aa89
Randy Steele <steelerx155@gmail.com>
2019-11-13 20:44:27 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -5,7 +5,7 @@ class ChattyStudent < Student
   puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
  end
  
-  def raise_hand
+  def raise_hand(pick)
     10.times puts { "Pick me!" }
   end
 end
